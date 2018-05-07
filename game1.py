e2926d17e78f9b9e278f45502abad311102f3275
jessicayu716 <38866606+jessicayu716@users.noreply.github.com>
2018-05-07 14:56:48 -0400

    Add files via upload

---
+++ game1.py
@@ -0,0 +1,20 @@
+from pygame import *
+from random import *
+from math import *
+import os
+from glob import *
+from getname import *
+
+screen=display.set_mode((600,800))
+
+running=True
+
+while running:
+    for e in event.get():
+        print(e)
+        if e.type==QUIT:
+            running=False
+
+
+    display.flip()
+quit()
